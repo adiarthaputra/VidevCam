7b68156bd1b04496557b51a1644577ec4e8d7987
adiarthaputra <124423360+adiarthaputra@users.noreply.github.com>
2026-06-05 14:55:22 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -5,10 +5,7 @@ export function Download() {
   const videoTitle = sessionStorage.getItem('videoTitle');
 
   const randomUrls = [
-    'https://otieu.com/4/10069080',
-    'https://lb.louieconurus.com/iv1GflMDYiQKXDaYJ/94691',
-    'https://aviatorreproducesauciness.com/2082665',
-    'https://viikqoye.com/dc/?blockID=388556'
+    'https://omg10.com/4/11101722',
   ];
   
   const handleDownload = () => {
